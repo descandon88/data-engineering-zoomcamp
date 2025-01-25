3a67908ebf32ff8aca246dfba847d37f3b68031d
David Escandon <descandon@cognus.cl>
2025-01-24 23:12:31 -0500

    adding variables.tf and variables.tf

--- 01-week/terrademo/main.tf
+++ 01-week/terrademo/main.tf
@@ -8,13 +8,18 @@ terraform {
 }
 
 provider "google" {
-  project = "helical-casing-353401"
-  region  = "us-central1"
+  #project = "helical-casing-353401"
+  credentials = file(var.credentials)
+  project     = var.project
+  #region  = "us-central1"
+  region = var.region
 }
 
 resource "google_storage_bucket" "demo-bucket" {
-  name          = "helical-casing-353401-terra-bucket"
-  location      = "US"
+  #name          = "helical-casing-353401-terra-bucket"
+  name = var.gcs_bucket_name
+  #location      = "US"
+  location      = var.location
   force_destroy = true
 
   lifecycle_rule {
@@ -29,4 +34,5 @@ resource "google_storage_bucket" "demo-bucket" {
 
 resource "google_bigquery_dataset" "demo_dataset" {
   dataset_id = "demo_dataset"
+  location   = var.location
 }
